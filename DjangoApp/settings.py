064f0192d7d5e848637a2c8b7934897ec2be108c
siboalpha <sibo.alphonsee@gmail.com>
2022-06-29 11:14:57 +0200

    Changing debug to false

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('MY_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['imbonizarwo.org', 'www.imbonizarwo.org', '97.107.142.215', '127.0.0.1']
 
