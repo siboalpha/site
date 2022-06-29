d48bcda9744c9a9b5eba871e325a870ef89ad645
siboalpha <sibo.alphonsee@gmail.com>
2022-06-29 11:21:15 +0200

    Bug fix

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-load_dotenv("/home/admin/site/DjangoApp")
+load_dotenv("/home/admin/site/DjangoApp/.env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
