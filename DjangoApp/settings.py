10e13ff26af252527aa068f9d0989402c10b3ffd
siboalpha <sibo.alphonsee@gmail.com>
2022-06-29 13:20:33 +0200

    Added team

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -1,10 +1,8 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+#load_dotenv()
 load_dotenv("/home/admin/site/DjangoApp/.env")
-
-import mimetypes
-mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
